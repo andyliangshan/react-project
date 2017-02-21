6a2921d704c44d9b744ad250fea9873afbdcfa26
evan2x <evan2zaw@gmail.com>
2017-02-21 15:28:13 +0800

    Update mock data

--- mock/index.js
+++ mock/index.js
@@ -1,23 +1,12 @@
 'use strict';
 
-module.exports = function(router) {
-  router.get('/api/user/:id', (req, res) => {
-    res.json({
-      status: 0,
-      data: {
-        name: 'evan2x',
-        avatar: 'https://avatars2.githubusercontent.com/u/10445139?v=3&s=460',
-        email: 'evan2zaw@gmail.com',
-        github: 'https://github.com/evan2x',
-        id: req.params.id
-      }
-    });
-  });
+const request = require('request');
 
-  router.get('/api/date', (req, res) => {
+module.exports = function(router) {
+  router.get('/api/hello', (req, res) => {
     res.json({
       status: 0,
-      data: new Date()
+      data: 'Hi, this is an example.'
     });
   });
 }
