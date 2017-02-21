46c41ceda130bd72a1c9c03baa815b6b2760be21
evan2x <evan2zaw@gmail.com>
2017-02-21 15:34:21 +0800

    Remove useless code in build\/config.js

--- build/config.js
+++ build/config.js
@@ -1,8 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-const path = require('path');
-
 const config = {
   title: 'React boilerplate',
   output: './dist',
@@ -29,33 +26,3 @@ config.proxy = {
 };
 
 module.exports = config;
-
-// const config = {
-//   title: '管理系统',
-//   inputBase: './src',
-//   outputBase: './dist',
-//   prod: {
-//     env: 'production',
-//     publicPath: '/assets/'
-//   },
-//   dev: {
-//     env: 'development',
-//     publicPath: '/',
-//     port: 8091,
-//     mock: {
-//       contentBase: './mock',
-//       port: 8092
-//     }
-//   }
-// };
-
-// // 开发环境代理配置
-// config.dev.proxy = {
-  // '/api/*': {
-  //   // 代理到mock server
-  //   target: `http://localhost:${config.dev.mock.port}/`,
-  //   secure: false
-  // }
-// };
-
-// module.exports = config;
